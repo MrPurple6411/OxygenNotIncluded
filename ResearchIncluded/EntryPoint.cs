52fc85a78a77e2215a32a9563132d7d688f5d2e6
MrPurple6411 <mrpurple6411@gmail.com>
2024-08-23 09:38:54 -0600

    ResearchIncluded which makes it so all techs are unlocked so long as it is enabled. (Techs revert if mod removed)
    Works on new games and saved games.

---
+++ ResearchIncluded/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿namespace ResearchIncluded;
+
+using HarmonyLib;
+using KMod;
+using UnityEngine;
+
+[HarmonyPatch]
+public class EntryPoint : UserMod2
+{
+    public override void OnLoad(Harmony harmony)
+    {
+        base.OnLoad(harmony);
+        Debug.Log("MrPurple6411's ResearchIncluded patched.");
+    }
+
+    [HarmonyPatch(typeof(Tech), nameof(Tech.IsComplete)), HarmonyPostfix]
+    public static void Postfix(Tech __instance, ref bool __result)
+    {
+        if (__result || Research.Instance == null)
+            return;
+
+        __result = true;
+    }
+}
