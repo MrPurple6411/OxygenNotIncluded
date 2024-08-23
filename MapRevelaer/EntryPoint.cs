21b48797810887d3b1c913af31adac3f700bf0e0
MrPurple6411 <mrpurple6411@gmail.com>
2024-08-23 09:36:29 -0600

    Convert to true revealer instead of just fog remover.

--- MapRevelaer/EntryPoint.cs
+++ MapRevelaer/EntryPoint.cs
@@ -3,6 +3,7 @@
 using HarmonyLib;
 using Klei;
 using KMod;
+using UnityEngine;
 
 [HarmonyPatch]
 public class EntryPoint : UserMod2
@@ -13,10 +14,29 @@ public class EntryPoint : UserMod2
         Debug.Log("MrPurple6411's Map Revealer patched.");
     }
 
-    [HarmonyPatch(typeof(GenericGameSettings), nameof(GenericGameSettings.disableFogOfWar), MethodType.Getter), HarmonyPrefix]
-    public static bool GenericGameSettings_disableFogOfWar_Getter_Prefix(ref bool __result)
+    //[HarmonyPatch(typeof(GenericGameSettings), nameof(GenericGameSettings.disableFogOfWar), MethodType.Getter), HarmonyPrefix]
+    //public static bool GenericGameSettings_disableFogOfWar_Getter_Prefix(ref bool __result)
+    //{
+    //    __result = true;
+    //    return false;
+    //}
+
+    [HarmonyPatch(typeof(GridVisibility), nameof(GridVisibility.Reveal)), HarmonyPostfix]
+    public static void GridVisibility_Reveal_Postfix(int baseX, int baseY)
     {
-        __result = true;
-        return false;
+        int num = (int)Grid.WorldIdx[baseY * Grid.WidthInCells + baseX];
+
+        for (int i = 0; i < Grid.HeightInCells; i++)
+        {
+            for (int j = 0; j < Grid.WidthInCells; j++)
+            {
+                int num4 = i * Grid.WidthInCells + j;
+                if (Grid.Visible[num4] < 255 && num == (int)Grid.WorldIdx[num4])
+                {
+                    Grid.Reveal(num4, 255, true);
+                }
+            }
+        }
+        SaveGame.Instance.worldGenSpawner.SpawnEverything();
     }
 }
